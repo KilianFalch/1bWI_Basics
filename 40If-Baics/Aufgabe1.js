1c639d3f818dfec98295c11cec121178b14b4281
Kilian <kilian@example.com>
2025-10-22 16:26:22 +0200

    Aufgabe 1 fertig gemacht

--- 40If-Baics/Aufgabe1.js
+++ 40If-Baics/Aufgabe1.js
@@ -1,1 +1,23 @@
+let a = 0
+
+
+//a > 10  positiv
+//a < 10  negativ
+//a = 10  gleich
+
+if (a > 0) {
+    console.log("The number is positive");
+    
+}
+    
+if (a < 0) {
+    console.log("The number is negative");
+    
+}
+
+if (a == 0) {
+    console.log("The number is zero");
+    
+}
+
 
