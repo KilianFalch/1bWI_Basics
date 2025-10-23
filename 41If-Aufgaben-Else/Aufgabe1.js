67f4acdba7c90889d346b179d355cec38c04aa8f
Kilian <kilian@example.com>
2025-10-23 19:19:26 +0200

    41 If Aufgaben Else fertig gemacht

---
+++ 41If-Aufgaben-Else/Aufgabe1.js
@@ -0,0 +1,18 @@
+let number = -4;
+
+//number > 0 positiv
+//number < 0 negativ
+//number == 0 gleich
+
+if (number > 0) {
+    console.log("the number is positive");
+    }    else ("The number is not positive");
+    
+
+if (number < 0) {
+    console.log("The number is negative");
+    }    else ("The number is positive or 0");
+if (number == 0 ) {
+    console.log("The number is zero");
+    }    else ("The number is not 0");    
+    
