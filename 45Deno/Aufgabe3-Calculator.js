f2cfa44666148fd9c2c2f98d3ea2df1fdf9120e3
Kilian <kilian@example.com>
2025-11-05 16:29:35 +0100

    45Deno Aufgabe 3 fertig gemacht

---
+++ 45Deno/Aufgabe3-Calculator.js
@@ -0,0 +1,34 @@
+let number1 = prompt("Gib die erste Zahl ein");
+let number2 = prompt("Gib die zweite Zahl ein");
+
+let number1AsNumber = parseInt(number1);
+let number2AsNumber = parseInt(number2);
+
+let selection = prompt("1 Add, 2 Subtract, 3 Multiplicate, 4 Divide");
+let selectionAsNumber = parseInt(selection);
+let result = 0;
+
+if (selectionAsNumber == 1) {
+
+    result = number1AsNumber + number2AsNumber;
+    console.log(result);
+    
+} else if (selectionAsNumber == 2) {
+    
+    result = number1AsNumber - number2AsNumber;
+    console.log(result);
+    
+} else if (selectionAsNumber == 3) {
+
+    result = number1AsNumber * number2AsNumber;
+    console.log(result);
+    
+}  else if (selectionAsNumber == 4) {
+
+    result = number1AsNumber / number2AsNumber;
+    console.log(result);
+    
+} else {
+    console.log("Not valid selection");
+    
+}
