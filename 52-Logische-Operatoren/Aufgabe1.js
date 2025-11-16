09016dba502c062c804cd817a452d034332465a6
Kilian <kilian@example.com>
2025-11-16 21:15:43 +0100

    52-Logische-Operatoren Aufgabe 1 feritg gemacht

---
+++ 52-Logische-Operatoren/Aufgabe1.js
@@ -0,0 +1,13 @@
+let temperature = 20
+
+let humidity = 90
+
+if (temperature >= 20 && temperature <= 25 && humidity <= 60) {
+
+    console.log("Perfect for outdoor activities");
+}else if (humidity >= 61 && humidity <= 80) {
+    console.log("Maybe stay indoors today");
+}else  {
+    
+    console.log("Consult a doctor before exercising");
+}
