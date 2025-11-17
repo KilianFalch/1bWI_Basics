4345c42feece8c5d42ec73ba01e2ada9139d70f2
Kilian <kilian@example.com>
2025-11-17 11:09:56 +0100

    52-Logische-Operatoren Aufgabe 3 fertig gemacht mit Daniel

---
+++ 52-Logische-Operatoren/Aufgabe3.js
@@ -0,0 +1,13 @@
+let grade = 89
+
+let attendence = 8
+
+let extraCredit = 9
+
+if (grade >= 80 && (attendence >= 90 || extraCredit >=5)) {
+  
+    console.log("Eligible for advanced exam");
+}else {
+
+    console.log("Not eglible for advanced exam");
+}
