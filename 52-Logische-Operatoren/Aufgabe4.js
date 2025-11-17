99bf1e31b4b2289df445fcba32fd6600eb46f060
Kilian <kilian@example.com>
2025-11-17 11:16:34 +0100

    52-Logische-Operatoren Aufgabe 4 feritg gemacht mit Daniel

---
+++ 52-Logische-Operatoren/Aufgabe4.js
@@ -0,0 +1,14 @@
+let age = 68
+
+let money = 9
+
+if (age >= 18 && money >= 12) {
+
+    console.log("Ticket purchased");
+}else if (age >= 18 && money <= 12) {
+ 
+    console.log("Not enough money");
+}else {
+
+    console.log("Too young to watch the movie");
+}
