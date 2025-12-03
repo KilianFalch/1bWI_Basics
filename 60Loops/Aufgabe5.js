607b7044ae767f34c85501dd32b9b2bd6bef57fa
Kilian <kilian@example.com>
2025-12-03 16:18:15 +0100

    60Loops Aufgabe 5 fertig gemacht

---
+++ 60Loops/Aufgabe5.js
@@ -0,0 +1,10 @@
+let x = parseInt(prompt("Gib hier die erste Zahl ein "))
+let y = parseInt(prompt("Gib hier die zweite Zahl ein "))
+let sum = 0;
+
+for (let i = x; i <= y; i++){
+
+    sum = sum + i;
+
+}  
+console.log(sum);
