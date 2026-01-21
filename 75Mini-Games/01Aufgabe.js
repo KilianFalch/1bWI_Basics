9e72f099b036465a3bc19b030b0c2c69ce6b611c
Kilian <kilian@example.com>
2026-01-21 15:36:22 +0100

    Aufgabe 1 fertig gemacht

--- 75Mini-Games/01Aufgabe.js
+++ 75Mini-Games/01Aufgabe.js
@@ -1,54 +1,48 @@
 let playing = true;
-while (playing == true){
+let again;
+let rounds;
+let player;
+let robot;
+let Spieler; 
+let Computer;
+while (playing){
 let x = prompt("Drücke Enter zum Würfeln")
 
-let Spieler = Math.floor(Math.random() * 6) + 1;
+ Spieler = Math.floor(Math.random() * 6) + 1;
 
-let Computer = Math.floor(Math.random() * 6) + 1;
+ Computer = Math.floor(Math.random() * 6) + 1;
 
+rounds += 1;
 
 if (Spieler > Computer) {
-    console.log("Spieler hat gewonnen");
-        
+    console.log("Spieler hat gewonnen die zahl vom spieler war  " + Spieler + " die zahl vom computer war " + Computer);
+    player += 1;
+    console.log(` es waren ${rounds} runden, du hast ${player} gewonnen`)
 }
 else if (Computer > Spieler) {
     
-console.log("Computer hat gewonnen")
+console.log("Computer hat gewonnen die zahl vom computer " + Computer + " die zahl vom spieler war " + Computer)
+    robot += 1;
+    console.log(` es waren ${rounds} runden, du hast ${player} gewonnen`)
 }else {
     
     console.log("Unentschieden");
+    console.log(` es waren ${rounds} runden, du hast ${player} gewonnen`)
 }
-
+if (rounds % 5 === 0 ){
+if(robot < player ){
+    console.log(" player hat gewonnen")
+}else if(robot > player){
+    console.log("computer hat gewonnen")
+}else {
+    console.log("es war unetschieden")
+}
 console.log(x)
-let again = prompt("if you want to do it again press Y/N");
+again = prompt("Wenn du nochmal spielen willsch drücke Y/N");
 if (again == "Y"){
     let playing = true;
 } else if (again =="N") {
     playing = false;
 }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
