5dd3c7575746e5558ce482f6961fd6951f7f87a7
Kilian <kilian@example.com>
2026-05-20 09:32:46 +0200

    Videos angeschaut

---
+++ 02_Web/03_Learning Skills mit JS/02_Videos/First Video/script.js
@@ -0,0 +1,14 @@
+document.getElementById("Button").addEventListener("click", function () {
+  const value1 = document.getElementById("value1").value;
+  const value2 = document.getElementById("value2").value;
+
+
+  if (isNaN(value1) || isNaN(value2)) {
+    alert("invalid value");
+  } else {
+    const sum = parseFloat(value1) + parseFloat(value2);
+    document.getElementById("result").innerText = "Sum: " + sum; 
+  }
+  
+  
+});
