58d93d52ed861ebc7489d56b9136e5e9ff9bdc81
Kilian <kilian@example.com>
2026-06-02 08:40:07 +0200

    Taschenrechner fertig gebaut und angefangen mit Generator

--- 02_Web/03_Learning Skills mit JS/02_Videos/First Video/script.js
+++ 02_Web/03_Learning Skills mit JS/02_Videos/First Video/script.js
@@ -6,8 +6,50 @@ document.getElementById("Button").addEventListener("click", function () {
   if (isNaN(value1) || isNaN(value2)) {
     alert("invalid value");
   } else {
-    const sum = parseFloat(value1) + parseFloat(value2);
-    document.getElementById("result").innerText = "Sum: " + sum; 
+    const product = parseFloat(value1) + parseFloat(value2);
+    document.getElementById("result").innerText = "Product: " + product; 
+  }
+  
+  
+});
+document.getElementById("Button2").addEventListener("click", function () {
+  const value1 = document.getElementById("value1").value;
+  const value2 = document.getElementById("value2").value;
+
+
+  if (isNaN(value1) || isNaN(value2)) {
+    alert("invalid value");
+  } else {
+    const product = parseFloat(value1) - parseFloat(value2);
+    document.getElementById("result").innerText = "Product: " + product; 
+  }
+  
+  
+});
+document.getElementById("Button3").addEventListener("click", function () {
+  const value1 = document.getElementById("value1").value;
+  const value2 = document.getElementById("value2").value;
+
+
+  if (isNaN(value1) || isNaN(value2)) {
+    alert("invalid value");
+  } else {
+    const product = parseFloat(value1) * parseFloat(value2);
+    document.getElementById("result").innerText = "Product: " + product; 
+  }
+  
+  
+});
+document.getElementById("Button4").addEventListener("click", function () {
+  const value1 = document.getElementById("value1").value;
+  const value2 = document.getElementById("value2").value;
+
+
+  if (isNaN(value1) || isNaN(value2)) {
+    alert("invalid value");
+  } else {
+    const product = parseFloat(value1) / parseFloat(value2);
+    document.getElementById("result").innerText = "Product: " + product; 
   }
   
   
